db4c078aa195d7981e21d4ae5560921a4fdaa232
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-03 21:00:23 +0500

    Resolve all bugs and learn all work does in the same commands there is no need to add separate commands as 1 2 3 named them & need only one IFFI function

--- Server_Bot/commands.js
+++ Server_Bot/commands.js
@@ -3,49 +3,35 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const command1 = [
+const commands = [
   {
     name: "ping",
     description: "Replies with Pong!",
   },
-];
-
-const rest = new REST({ version: "10" }).setToken(
-  process.env.DISCORD_BOT_TOKEN,
-);
-
-(async () => {
-  try {
-    console.log("Started refreshing application (/) commmands.");
-    await rest.put(
-      Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
-      { body: command1 }
-    );
-    console.log("Succesfully reloaded");
-  } catch (e) {
-    console.error(e);
-  }
-})();
-
-
-// Create Short URL
-
-
-const command2 = [
   {
     name: "url",
     description: "Send url that you send me for just fun & testing",
+    options: [
+      {
+        name: "url",
+        description: "Enter your URL",
+        type: 3, // 3 = STRING
+        required: true,
+      },
+    ],
   },
 ];
 
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN);
+
 (async () => {
   try {
-    console.log("Started refreshing application (/) commmands.");
+    console.log("Started refreshing application (/) commands.");
     await rest.put(
       Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
-      { body: command2 }
+      { body: commands }
     );
-    console.log("Succesfully reloaded");
+    console.log("Successfully reloaded all application (/) commands.");
   } catch (e) {
     console.error(e);
   }
